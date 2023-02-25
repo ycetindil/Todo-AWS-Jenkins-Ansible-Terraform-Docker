7e80412ed82a85df77e8d07e9da39a2253e6abca
Yasin Cetindil <ycetindil@gmail.com>
2023-02-25 14:09:43 -0800

    Test

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
 output "react_ip" {
-  value = "http://${aws_instance.managed_nodes[2].public_ip}:3000"
+  value = "http://${aws_instance.ec2_instance[2].public_ip}:3000"
 }
 
 output "nodejs_public_ip" {
-  value = aws_instance.managed_nodes[1].public_ip
+  value = aws_instance.ec2_instance[1].public_ip
 }
 
 output "postgresql_private_ip" {
-  value = aws_instance.managed_nodes[0].private_ip
+  value = aws_instance.ec2_instance[0].private_ip
 }
